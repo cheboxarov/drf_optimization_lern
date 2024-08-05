9acedefaa147b259773d7eecd5c89da4d1590173
wlovem <lalakasuper2@gmail.com>
2024-08-05 14:54:54 +0700

    Added annotate sum and aggregate total sum

--- service/services/views.py
+++ service/services/views.py
@@ -4,11 +4,30 @@ from .models import Subscription
 from django.db.models import Prefetch
 from .serializers import SubscriptionSerializer
 from clients.models import Client
+from django.db.models import F, Sum
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related(
+    queryset = (Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all()
                  .select_related('user').only('company_name', 'user__email')))
+                .annotate(price=F('service__full_price') - F('service__full_price') *
+                                (F('plan__discount_percent') / 100.00)))
 
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+
+        queryset = self.get_queryset()
+
+        total_amount = queryset.aggregate(total=Sum('price'))["total"]
+
+        response_data = {
+            'total_amount': total_amount,
+            'result': response.data
+        }
+        response.data = response_data
+
+        return response
